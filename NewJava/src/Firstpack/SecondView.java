e7c0d337768ca57c42ec3c7c097c4ccee46fb17a
vinodcse99 <vinod.gogula6@gmail.com>
2020-03-10 17:11:25 +0530

    newly added

---
+++ NewJava/src/Firstpack/SecondView.java
@@ -0,0 +1,10 @@
+package Firstpack;
+
+public class SecondView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is added just now");
+	}
+
+}
