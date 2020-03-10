87af41fcd4c4c1c3c2abe60e8480af723bb3cbc8
vinodcse99 <vinod.gogula6@gmail.com>
2020-03-10 17:04:23 +0530

    this is my first commit

---
+++ NewJava/src/Firstpack/Welcome.java
@@ -0,0 +1,12 @@
+package Firstpack;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("welcome to java world");
+	}
+
+}
